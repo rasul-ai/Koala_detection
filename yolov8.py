5bc53bb4e6ad994e33a9127ee0ae3c03faa29632
Md Rasul Islam Bapary <ai.rasulbapary@gmail.com>
2023-11-25 20:48:35 +0600

    Update yolov8.py

--- yolov8.py
+++ yolov8.py
@@ -1,9 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("/home/bapary/Documents/Medical/runs/detect/train/weights/best.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
 model.train(data="./custom.yaml", epochs=100, imgsz=200)  # train the model
 # metrics = model.val()  # evaluate model performance on the validation set
 # results = model.predict(source="/home/bapary/Videos/koala/Cartoon", save=True)  # predict on an image
-# success = model.export(format="")  # export the model to ONNX format

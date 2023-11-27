e944425e6ae393f6d3e9a65210eb6d7d2af450bb
Md Rasul Islam Bapary <ai.rasulbapary@gmail.com>
2023-11-27 16:26:17 +0600

    Delete yolov8.py

--- yolov8.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("/home/bapary/Documents/Medical/runs/detect/train/weights/best.pt")  # load a pretrained model (recommended for training)
-
-# Use the model
-model.train(data="./custom.yaml", epochs=100, imgsz=200)  # train the model
-# metrics = model.val()  # evaluate model performance on the validation set
-# results = model.predict(source="/home/bapary/Videos/koala/Cartoon", save=True)  # predict on an image
